d732c83da37409f0f78c8c448b770611f3695bcc
James Sargant <shorinbonsai@gmail.com>
2022-11-25 20:04:08 -0500

    error section complete

---
+++ errors/wallet_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBaalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
+		assertBaalance(t, wallet, Bitcoin(10))
+
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+		assertError(t, err, ErrInsuficientFunds)
+		assertBaalance(t, wallet, startingBalance)
+	})
+}
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted error, but none found")
+	}
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+func assertBaalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
